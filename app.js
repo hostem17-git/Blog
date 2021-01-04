cb4935d662da14aa308d8e966d27dec8b6a9de60
hostem17-git <vjjais17@gmai.com>
2021-01-04 18:00:31 +0530

    Challenge 4 Done

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -12,13 +10,16 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-
-
-
-
+app.get("/", function(req, res) {
+  res.render("home",{
+    homeStartingContent:homeStartingContent
+  });
+})
 
 
 
